98fad5048dc983e80889db71c7ec4457e7ed11c8
Satoshi Nishimura <nishim314@gmail.com>
2016-07-07 08:35:51 +0900

    add manyTill parser

--- test/ParserTest.php
+++ test/ParserTest.php
@@ -15,6 +15,8 @@ use function Laiz\Parsec\satisfy;
 use function Laiz\Parsec\label;
 use function Laiz\Parsec\labels;
 use function Laiz\Parsec\unexpected;
+use function Laiz\Parsec\anyToken;
+use function Laiz\Parsec\manyTill;
 use function Laiz\Func\Either\Right;
 use function Laiz\Func\Either\Left;
 use function Laiz\Func\Alternative\aor;
@@ -152,5 +154,25 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 
         $ret = parse($parser, "Test", "abAB");
         $this->assertEquals(Right('abAB'), $ret);
+    }
+
+    function testManyTill()
+    {
+        $end = str('EOF;');
+        $parser = manyTill(anyToken(), $end);
+
+        $ret = parse($parser, "Test", "abcEOF;a");
+        $this->assertEquals(Right(['a', 'b', 'c']), $ret);
+
+        $ret = parse($parser, "Test", "EOF;a");
+        $this->assertEquals(Right([]), $ret);
+
+        $ret = parse($parser, "Test", "abc");
+        $err = null;
+        $ret->either(function($a) use (&$err){
+            $err = $a;
+        }, function($a){});
+        $this->assertRegExp("/unexpected end of input/", show($err));
+        $this->assertRegExp("/expecting EOF/", show($err));
     }
 }
