35266b814ba9efecda38cbbbbbf52280b4cbe660
Satoshi Nishimura <nishim314@gmail.com>
2016-07-11 18:06:24 +0900

    add aempty test

--- test/TypeClassTest.php
+++ test/TypeClassTest.php
@@ -8,6 +8,7 @@ use function Laiz\Func\Functor\fmap;
 use function Laiz\Func\Applicative\ap;
 use function Laiz\Func\Monoid\mempty;
 use function Laiz\Func\MonadPlus\mzero;
+use function Laiz\Func\Alternative\aempty;
 use function Laiz\Parsec\parse;
 use function Laiz\Parsec\str;
 use function Laiz\Parsec\Show\show;
@@ -26,6 +27,14 @@ class TypeClass extends \PHPUnit_Framework_TestCase
         }, function($a){});
         $this->assertRegExp("/unknown parse error/", show($err));
 
+
+        $parser = str('ab')->mappend(aempty());
+        $ret = parse($parser, "Test", "abcd");
+        $err = null;
+        $ret->either(function($a) use (&$err){
+            $err = $a;
+        }, function($a){});
+        $this->assertRegExp("/unknown parse error/", show($err));
     }
 
     public function testFmapAp()
